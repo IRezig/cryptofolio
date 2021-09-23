c059848777c386bc26d8a03da173bd18239ddd17
rezig <rezig.ibtissam@gmail.com>
2021-09-23 11:09:59 +0200

    install dependencies

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
+import { Link } from 'react-router-dom'
+import { HomeOutLined  } from '@ant-design/icons'
+
+export default function Navbar() {
+  return (
+    <div className='nav-container'>
+      <div className='logo-container'>
+      </div>
+    </div>
+  )
+}
